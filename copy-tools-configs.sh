0666d7bc3b564624e7ff613059b1786243bb1ad8
Christian Convey <christian.convey@gmail.com>
2018-08-15 09:41:13 -0400

    fix

--- copy-tools-configs.sh
+++ copy-tools-configs.sh
@@ -11,7 +11,17 @@ cp -r "${SRC_ROOT}/dot-vim"          ~/.vim
 cp    "${SRC_ROOT}/gdbinit"          ~/.gdbinit
 cp    "${SRC_ROOT}/gitconfig"        ~/.gitconfig
 cp    "${SRC_ROOT}/gitignore_global" ~/.gitignore_global
+
+if [[ -f ~/.vimrc ]]; then
+    cp ~/.vimrc ~/.vimrc.bak
+    rm ~/.vimrc
+fi
 ln -s "${SRC_ROOT}/vimrc"            ~/.vimrc
+
+if [[ -f ~/.bashrc_common.sh ]]; then
+    cp ~/.bashrc_common.sh ~/.bashrc_common.sh.bak
+    rm ~/.bashrc_common.sh
+fi
 ln -s "${SRC_ROOT}/bashrc_common.sh" ~/.bashrc_common
 
 mkdir -p ~/bin
